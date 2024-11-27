19bb4b5e214a116a18d196adfc56760648730c53
Lawrence Eniola <lawrencekunmilola@gmail.com>
2024-11-27 15:37:14 +0100

    fix gitignore

--- backend/dist/controllers/user.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.allAccess = (req, res) => {
-    res.status(200).send("Public Content.");
-};
-exports.userBoard = (req, res) => {
-    res.status(200).send("User Content.");
-};
